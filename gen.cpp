932c60f70b8eb9fdf4e82f9ef4fb6f6a0cbaac22
ashleyhlai <ahlai@cmu.edu>
2016-04-19 20:32:10 -0400

    Flipped num_vars and num_clauses

--- gen.cpp
+++ gen.cpp
@@ -27,10 +27,10 @@ std::string gen_sat(int num_vars, int num_clauses, int max_clause_len) {
   for (int i = 0; i < num_clauses; i++) {
     int clause_len = rand() % max_clause_len + 1;
     for (int j = 1; j < clause_len; j++) {
-      expr += get_rand_var(num_clauses);
+      expr += get_rand_var(num_vars);
       expr += ",";
     }
-    expr += get_rand_var(num_clauses);
+    expr += get_rand_var(num_vars);
     expr += "  ";
   }
   return expr;
@@ -38,7 +38,7 @@ std::string gen_sat(int num_vars, int num_clauses, int max_clause_len) {
 
 // input form of SAT expression: 2,1 -2,3,-4
 int main(int argc, char** argv) {
-  std::string result = gen_sat(10, 10, 5);
+  std::string result = gen_sat(100, 10, 5);
   std::cout << result << "\n";
   return 1;
 }
